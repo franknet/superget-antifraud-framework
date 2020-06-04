7e178fef8d3eedeb36fe27e3650590161bfe3ec4
Leandro Lopes <leandrolopes.datum@getnet.com.br>
2020-06-04 16:47:24 -0300

    feature(GPAY-1151): saving Account on UserDefaults

--- Getpay-Core/Modules/Utils/GPUtils.swift
+++ Getpay-Core/Modules/Utils/GPUtils.swift
@@ -2,12 +2,35 @@ import Foundation
 
 public class GPUtils {
     
+    private static let terminalKey = "terminalKey"
+    private static let accountKey = "accountKey"
+    private static let merchantKey = "merchantKey"
+    
+    public static func save(accountInUD account: GPAccount) {
+        let encoder = JSONEncoder()
+        if let encoded = try? encoder.encode(account) {
+            let defaults = UserDefaults.shared
+            defaults.set(encoded, forKey: accountKey)
+        }
+    }
+    
+    public static func loadAccountFromUD() -> GPAccount? {
+        if let account = UserDefaults.shared.object(forKey: accountKey) as? Data {
+            let decoder = JSONDecoder()
+            if let loadedAccount = try? decoder.decode(GPAccount.self, from: account) {
+                return loadedAccount
+            }
+        }
+        
+        return nil
+    }
+    
     public static func saveTerminalInUD(terminal: String) {
-        UserDefaults.shared.set(terminal, forKey: "terminalKey")
+        UserDefaults.shared.set(terminal, forKey: terminalKey)
     }
 
     public static func loadTerminalFromUD() -> String {
-        if let terminal = UserDefaults.shared.string(forKey: "terminalKey") {
+        if let terminal = UserDefaults.shared.string(forKey: terminalKey) {
             return terminal
         }
         return ""
@@ -17,12 +40,12 @@ public class GPUtils {
         let encoder = JSONEncoder()
         if let encoded = try? encoder.encode(merchant) {
             let defaults = UserDefaults.shared
-            defaults.set(encoded, forKey: "merchantKey")
+            defaults.set(encoded, forKey: merchantKey)
         }
     }
 
     public static func loadGPMerchantFromUD() -> GPMerchant {
-        if let merchant = UserDefaults.shared.object(forKey: "merchantKey") as? Data {
+        if let merchant = UserDefaults.shared.object(forKey: merchantKey) as? Data {
             let decoder = JSONDecoder()
             if let loadedMerchant = try? decoder.decode(GPMerchant.self, from: merchant) {
                 return loadedMerchant
