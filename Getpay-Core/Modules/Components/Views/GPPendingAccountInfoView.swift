e02badd66f96b65a1c1dbbeff49c15aaefac7dbc
Leandro Lopes <leandrolopes.datum@getnet.com.br>
2020-06-08 16:46:33 -0300

    feature(GPAY-1472): code review

--- Getpay-Core/Modules/Components/Views/GPPendingAccountInfoView.swift
+++ Getpay-Core/Modules/Components/Views/GPPendingAccountInfoView.swift
@@ -1,68 +1,6 @@
 
 import UIKit
 
-// MARK: - Protocol
-private protocol PendingViewPreset {
-    
-    var title: String { get }
-    var subTitle: String { get }
-    var image: String? { get }
-    var buttonTitle: String { get }
-    
-}
-
-// MARK: - Struct
-private struct PresetNewClient404: PendingViewPreset {
-    
-    var title: String = "Abra sua conta para fazer transferências"
-    
-    var subTitle: String = "Para utilizar essa funcionalidade, é necessário abrir uma conta SuperGet."
-    
-    var image: String? = nil
-    
-    var buttonTitle: String = "ABRIR CONTA"
-    
-}
-
-// MARK: - Struct
-private struct PresetClient403: PendingViewPreset {
-
-    var title: String = "Falha ao carregar as informações"
-    
-    var subTitle: String = "Não foi possível carregar os dados da sua conta. Por favor, tente novamente."
-    
-    var image: String? = "gp_error"
-    
-    var buttonTitle: String = "TENTAR NOVAMENTE"
-    
-}
-
-// MARK: - Struct
-private struct PresetWaitingDocumentsNewClient: PendingViewPreset {
-
-    var title: String = "Complete seu cadastro para fazer transferências"
-    
-    var subTitle: String = "Para utilizar essa funcionalidade, é necessário que você envie os documentos necessários."
-    
-    var image: String? = nil
-    
-    var buttonTitle: String = "Abrir Conta"
-    
-}
-
-// MARK: - Struct
-private struct PresetGeneric: PendingViewPreset {
-
-    var title: String = "Abra sua conta para fazer transferências"
-    
-    var subTitle: String = "Para utilizar essa funcionalidade, é necessário abrir uma conta SuperGet."
-    
-    var image: String? = nil
-    
-    var buttonTitle: String = "Abrir Conta"
-    
-}
-
 // MARK: - Class
 public class GPPendingAccountInfoView: UIView {
     // MARK: - Private variables
