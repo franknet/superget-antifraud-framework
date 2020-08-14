c3e6c8277aa4a75f5fbf61c9d8b2f02fd419f7a0
Marcus Vinicius Grussner <marcusviniciusgrussner@Marcuss-MacBook-Pro-2.local>
2020-08-14 16:31:52 -0300

    add new warning label

--- Getpay-Core/Modules/Components/Receipt/CommonViews/GPReceiptPaymentStatusView.swift
+++ Getpay-Core/Modules/Components/Receipt/CommonViews/GPReceiptPaymentStatusView.swift
@@ -13,15 +13,24 @@ public class GPReceiptPaymentStatusView: UIView {
         return stack
     }()
     
-    private lazy var title = GPLabel(fontSize: 12.0, alignment: .center, textColor: GPColors.milhouse.color)
+    private lazy var borderTopView = UIView()
+    
+    private lazy var contentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = GPColors.lenny.color
+        return view
+    }()
+    
+    private lazy var title = GPLabel(fontSize: 14.0, alignment: .left, textColor: GPColors.flanders.color)
     
     // MARK: - Initializers
     
-    public init() {
+    public init(borderTopViewColor: UIColor? = GPColors.lenny.color) {
         super.init(frame: .zero)
         
         addComponents()
         setupConstraints()
+        borderTopView.backgroundColor = borderTopViewColor
     }
     
     required init?(coder _: NSCoder) {
@@ -32,7 +41,6 @@ public class GPReceiptPaymentStatusView: UIView {
     
     public func configure(title: String) {
         self.title.text = title
-        self.title.backgroundColor = GPColors.lenny.color
     }
 
     
@@ -40,15 +48,24 @@ public class GPReceiptPaymentStatusView: UIView {
     
     private func addComponents() {
         addSubviews([stack], constraints: true)
-        stack.addArrangedSubview(title)
+        stack.addArrangedSubview(borderTopView)
+        stack.addArrangedSubview(contentView)
+        
+        contentView.addSubview(title, constraints: true)
     }
 
     private func setupConstraints() {
-        title.topAnchor.constraint(equalTo: topAnchor, constant: 8.0).isActive = true
-        title.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8.0).isActive = true
-        title.heightAnchor.constraint(equalToConstant: 30).isActive = true
-        title.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
-        title.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        borderTopView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        borderTopView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        borderTopView.heightAnchor.constraint(equalToConstant: 12.0).isActive = true
+        
+        contentView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        contentView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        
+        title.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16.0).isActive = true
+        title.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16.0).isActive = true
+        title.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16.0).isActive = true
+        title.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16.0).isActive = true
         
         stack.topAnchor.constraint(equalTo: topAnchor).isActive = true
         stack.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
