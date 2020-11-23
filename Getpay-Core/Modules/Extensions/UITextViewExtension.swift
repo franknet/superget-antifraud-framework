3dafaa2e53c119efb5eb8b958365899b553dda5d
Ludgero Gil Mascarenhas <ludgeromascarenhas_datum@getnet.com.br>
2020-11-23 09:36:30 -0300

    terms service added

---
+++ Getpay-Core/Modules/Extensions/UITextViewExtension.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+// MARK: - UITextView Extension
+
+public extension UITextView {
+    func centerText() {
+        self.textAlignment = .center
+        let fittingSize = CGSize(width: bounds.width, height: CGFloat.greatestFiniteMagnitude)
+        let size = sizeThatFits(fittingSize)
+        let topOffset = (bounds.size.height - size.height * zoomScale) / 2
+        let positiveTopOffset = max(1, topOffset)
+        contentOffset.y = -positiveTopOffset
+    }
+}
---
+++ Getpay-Core/Modules/Terms Service/GNFingerPrint.swift
@@ -0,0 +1,59 @@
+import UIKit
+
+// MARK: - Class
+
+public class GNFingerPrint {
+    
+    // MARK: - Initializers
+    
+    public init() {}
+    
+    // MARK: - Public methods
+    
+    public func getAddress() -> String {
+        let wifiIP = getAddress(for: .wifi)
+        if wifiIP.isNotEmpty {
+            return wifiIP
+        }
+        return getAddress(for: .cellular)
+    }
+}
+
+extension GNFingerPrint {
+    
+    // MARK: - Private methods
+    
+    private func getAddress(for network: Network) -> String {
+        var address = ""
+        var ifaddr: UnsafeMutablePointer<ifaddrs>?
+        guard getifaddrs(&ifaddr) == 0 else { return "" }
+        guard let firstAddr = ifaddr else { return "" }
+        
+        for ifptr in sequence(first: firstAddr, next: { $0.pointee.ifa_next }) {
+            let interface = ifptr.pointee
+            
+            let addrFamily = interface.ifa_addr.pointee.sa_family
+            if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
+                
+                let name = String(cString: interface.ifa_name)
+                if name == network.rawValue {
+                    var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                    getnameinfo(interface.ifa_addr, socklen_t(interface.ifa_addr.pointee.sa_len),
+                                &hostname, socklen_t(hostname.count),
+                                nil, socklen_t(0), NI_NUMERICHOST)
+                    address = String(cString: hostname)
+                }
+            }
+        }
+        freeifaddrs(ifaddr)
+        return address
+    }
+    
+}
+
+// MARK: - Enum
+
+public enum Network: String {
+    case wifi = "en0"
+    case cellular = "pdp_ip0"
+}
