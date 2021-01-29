e0615b04a7b05392de6ebe19162a02256f4a2e75
Marcus Vinicius Grussner <marcusviniciusgrussner@Marcuss-MacBook-Pro-2.local>
2021-01-29 15:02:01 -0300

    add country enum

---
+++ Getpay-Core/Modules/Models/GNCountry.swift
@@ -0,0 +1,505 @@
+public enum GNCountry {
+    case AF
+    case ZA
+    case AX
+    case AL
+    case DE
+    case AD
+    case AO
+    case AI
+    case AQ
+    case AG
+    case SA
+    case DZ
+    case AR
+    case AM
+    case AW
+    case AU
+    case AT
+    case AZ
+    case BS
+    case BD
+    case BB
+    case BH
+    case BE
+    case BZ
+    case BJ
+    case BM
+    case BY
+    case BO
+    case BQ
+    case BA
+    case BW
+    case BV
+    case BR
+    case BN
+    case BG
+    case BF
+    case BI
+    case BT
+    case CV
+    case KH
+    case CM
+    case CA
+    case QA
+    case KY
+    case KZ
+    case CF
+    case TD
+    case CZ
+    case CL
+    case CN
+    case CY
+    case CX
+    case CC
+    case CO
+    case KM
+    case CG
+    case CD
+    case CK
+    case KR
+    case KP
+    case CI
+    case CR
+    case HR
+    case CU
+    case CW
+    case DK
+    case DJ
+    case DM
+    case DO
+    case EG
+    case SV
+    case AE
+    case EC
+    case ER
+    case SK
+    case SI
+    case ES
+    case US
+    case EE
+    case SZ
+    case ET
+    case FO
+    case FJ
+    case PH
+    case FI
+    case FR
+    case GA
+    case GM
+    case GH
+    case GE
+    case GS
+    case GI
+    case GR
+    case GD
+    case GL
+    case GP
+    case GU
+    case GT
+    case GG
+    case GY
+    case GF
+    case GW
+    case GN
+    case GQ
+    case HT
+    case HM
+    case HN
+    case HK
+    case HU
+    case YE
+    case IN
+    case ID
+    case IQ
+    case IR
+    case IE
+    case IS
+    case IL
+    case IT
+    case JM
+    case JP
+    case JE
+    case JO
+    case KI
+    case KW
+    case LA
+    case LS
+    case LV
+    case LB
+    case LR
+    case LY
+    case LI
+    case LT
+    case LU
+    case MO
+    case MK
+    case MG
+    case MY
+    case MW
+    case MV
+    case ML
+    case MT
+    case FK
+    case IM
+    case MP
+    case MA
+    case MH
+    case MQ
+    case MU
+    case MR
+    case YT
+    case UM
+    case MX
+    case MM
+    case FM
+    case MZ
+    case MD
+    case MC
+    case MN
+    case ME
+    case MS
+    case NA
+    case NR
+    case NP
+    case NI
+    case NE
+    case NG
+    case NU
+    case NF
+    case NO
+    case NC
+    case NZ
+    case OM
+    case NL
+    case PW
+    case PS
+    case PA
+    case PG
+    case PK
+    case PY
+    case PE
+    case PN
+    case PF
+    case PL
+    case PR
+    case PT
+    case KE
+    case KG
+    case GB
+    case RE
+    case RO
+    case RW
+    case RU
+    case EH
+    case AS
+    case WS
+    case PM
+    case SB
+    case SM
+    case SH
+    case LC
+    case BL
+    case KN
+    case SX
+    case MF
+    case ST
+    case VC
+    case SN
+    case SL
+    case RS
+    case SC
+    case SG
+    case SY
+    case SO
+    case LK
+    case SD
+    case SS
+    case SE
+    case CH
+    case SR
+    case SJ
+    case TH
+    case TW
+    case TJ
+    case TZ
+    case TF
+    case IO
+    case TL
+    case TG
+    case TK
+    case TO
+    case TT
+    case TN
+    case TC
+    case TM
+    case TR
+    case TV
+    case UA
+    case UG
+    case UY
+    case UZ
+    case VU
+    case VA
+    case VE
+    case VN
+    case VI
+    case VG
+    case WF
+    case ZM
+    case ZW
+    
+    public var countryName: String {
+        switch self {
+        case .AF: return "Afeganistão"
+        case .ZA: return "África do Sul"
+        case .AX: return "Åland, Ilhas"
+        case .AL: return "Albânia"
+        case .DE: return "Alemanha"
+        case .AD: return "Andorra"
+        case .AO: return "Angola"
+        case .AI: return "Anguilla"
+        case .AQ: return "Antártida"
+        case .AG: return "Antígua e Barbuda"
+        case .SA: return "Arábia Saudita"
+        case .DZ: return "Argélia"
+        case .AR: return "Argentina"
+        case .AM: return "Armênia"
+        case .AW: return "Aruba"
+        case .AU: return "Austrália"
+        case .AT: return "Áustria"
+        case .AZ: return "Azerbaijão"
+        case .BS: return "Bahamas"
+        case .BD: return "Bangladesh"
+        case .BB: return "Barbados"
+        case .BH: return "Barém"
+        case .BE: return "Bélgica"
+        case .BZ: return "Belize"
+        case .BJ: return "Benim"
+        case .BM: return "Bermudas"
+        case .BY: return "Bielorrússia"
+        case .BO: return "Bolívia"
+        case .BQ: return "Bonaire, Santo Eustáquio e Saba"
+        case .BA: return "Bósnia e Herzegovina"
+        case .BW: return "Botswana"
+        case .BV: return "Bouvet, Ilha"
+        case .BR: return "Brasil"
+        case .BN: return "Brunei"
+        case .BG: return "Bulgária"
+        case .BF: return "Burkina Faso"
+        case .BI: return "Burundi"
+        case .BT: return "Butão"
+        case .CV: return "Cabo Verde"
+        case .KH: return "Cambodja"
+        case .CM: return "Camarões"
+        case .CA: return "Canadá"
+        case .QA: return "Catar"
+        case .KY: return "Cayman, Ilhas"
+        case .KZ: return "Cazaquistão"
+        case .CF: return "Centro-Africana, República"
+        case .TD: return "Chade"
+        case .CZ: return "Chéquia"
+        case .CL: return "Chile"
+        case .CN: return "China"
+        case .CY: return "Chipre"
+        case .CX: return "Christmas, Ilha"
+        case .CC: return "Cocos, Ilhas"
+        case .CO: return "Colômbia"
+        case .KM: return "Comores"
+        case .CG: return "Congo, República do"
+        case .CD: return "Congo, República Democrática do (antigo Zaire)"
+        case .CK: return "Cook, Ilhas"
+        case .KR: return "Coreia do Sul"
+        case .KP: return "Coreia, República Democrática da (Coreia do Norte)"
+        case .CI: return "Costa do Marfim"
+        case .CR: return "Costa Rica"
+        case .HR: return "Croácia"
+        case .CU: return "Cuba"
+        case .CW: return "Curaçao"
+        case .DK: return "Dinamarca"
+        case .DJ: return "Djibouti"
+        case .DM: return "Dominica"
+        case .DO: return "Dominicana, República"
+        case .EG: return "Egito"
+        case .SV: return "El Salvador"
+        case .AE: return "Emirados Árabes Unidos"
+        case .EC: return "Equador"
+        case .ER: return "Eritreia"
+        case .SK: return "Eslováquia"
+        case .SI: return "Eslovénia"
+        case .ES: return "Espanha"
+        case .US: return "Estados Unidos"
+        case .EE: return "Estónia"
+        case .SZ: return "Essuatíni"
+        case .ET: return "Etiópia"
+        case .FO: return "Feroé, Ilhas"
+        case .FJ: return "Fiji"
+        case .PH: return "Filipinas"
+        case .FI: return "Finlândia"
+        case .FR: return "França"
+        case .GA: return "Gabão"
+        case .GM: return "Gâmbia"
+        case .GH: return "Gana"
+        case .GE: return "Geórgia"
+        case .GS: return "Geórgia do Sul e Sandwich do Sul, Ilhas"
+        case .GI: return "Gibraltar"
+        case .GR: return "Grécia"
+        case .GD: return "Grenada"
+        case .GL: return "Groenlândia"
+        case .GP: return "Guadalupe"
+        case .GU: return "Guam"
+        case .GT: return "Guatemala"
+        case .GG: return "Guernsey"
+        case .GY: return "Guiana"
+        case .GF: return "Guiana Francesa"
+        case .GW: return "Guiné-Bissau"
+        case .GN: return "Guiné-Conacri"
+        case .GQ: return "Guiné Equatorial"
+        case .HT: return "Haiti"
+        case .HM: return "Heard e Ilhas McDonald, Ilha"
+        case .HN: return "Honduras"
+        case .HK: return "Hong Kong"
+        case .HU: return "Hungria"
+        case .YE: return "Iémen"
+        case .IN: return "Índia"
+        case .ID: return "Indonésia"
+        case .IQ: return "Iraque"
+        case .IR: return "Irã"
+        case .IE: return "Irlanda"
+        case .IS: return "Islândia"
+        case .IL: return "Israel"
+        case .IT: return "Itália"
+        case .JM: return "Jamaica"
+        case .JP: return "Japão"
+        case .JE: return "Jersey"
+        case .JO: return "Jordânia"
+        case .KI: return "Kiribati"
+        case .KW: return "Kuwait"
+        case .LA: return "Laos"
+        case .LS: return "Lesoto"
+        case .LV: return "Letônia"
+        case .LB: return "Líbano"
+        case .LR: return "Libéria"
+        case .LY: return "Líbia"
+        case .LI: return "Liechtenstein"
+        case .LT: return "Lituânia"
+        case .LU: return "Luxemburgo"
+        case .MO: return "Macau"
+        case .MK: return "Macedônia do Norte"
+        case .MG: return "Madagáscar"
+        case .MY: return "Malásia"
+        case .MW: return "Malawi"
+        case .MV: return "Maldivas"
+        case .ML: return "Mali"
+        case .MT: return "Malta"
+        case .FK: return "Malvinas, Ilhas (Falkland)"
+        case .IM: return "Man, Ilha de"
+        case .MP: return "Marianas Setentrionais"
+        case .MA: return "Marrocos"
+        case .MH: return "Marshall, Ilhas"
+        case .MQ: return "Martinica"
+        case .MU: return "Maurícia"
+        case .MR: return "Mauritânia"
+        case .YT: return "Mayotte"
+        case .UM: return "Menores Distantes dos Estados Unidos, Ilhas"
+        case .MX: return "México"
+        case .MM: return "Myanmar (antiga Birmânia)"
+        case .FM: return "Micronésia, Estados Federados da"
+        case .MZ: return "Moçambique"
+        case .MD: return "Moldávia"
+        case .MC: return "Mônaco"
+        case .MN: return "Mongólia"
+        case .ME: return "Montenegro"
+        case .MS: return "Montserrat"
+        case .NA: return "Namíbia"
+        case .NR: return "Nauru"
+        case .NP: return "Nepal"
+        case .NI: return "Nicarágua"
+        case .NE: return "Níger"
+        case .NG: return "Nigéria"
+        case .NU: return "Niue"
+        case .NF: return "Norfolk, Ilha"
+        case .NO: return "Noruega"
+        case .NC: return "Nova Caledônia"
+        case .NZ: return "Nova Zelândia (Aotearoa)"
+        case .OM: return "Oman"
+        case .NL: return "Países Baixos (Holanda)"
+        case .PW: return "Palau"
+        case .PS: return "Palestina"
+        case .PA: return "Panamá"
+        case .PG: return "Papua-Nova Guiné"
+        case .PK: return "Paquistão"
+        case .PY: return "Paraguai"
+        case .PE: return "Peru"
+        case .PN: return "Pitcairn"
+        case .PF: return "Polinésia Francesa"
+        case .PL: return "Polônia"
+        case .PR: return "Porto Rico"
+        case .PT: return "Portugal"
+        case .KE: return "Quênia"
+        case .KG: return "Quirguistão"
+        case .GB: return "Reino Unido da Grã-Bretanha e Irlanda do Norte"
+        case .RE: return "Reunião"
+        case .RO: return "Romênia"
+        case .RW: return "Ruanda"
+        case .RU: return "Rússia"
+        case .EH: return "Saara Ocidental"
+        case .AS: return "Samoa Americana"
+        case .WS: return "Samoa (Samoa Ocidental)"
+        case .PM: return "Saint Pierre et Miquelon"
+        case .SB: return "Salomão, Ilhas"
+        case .SM: return "San Marino"
+        case .SH: return "Santa Helena"
+        case .LC: return "Santa Lúcia"
+        case .BL: return "São Bartolomeu"
+        case .KN: return "São Cristóvão e Névis (Saint Kitts e Nevis)"
+        case .SX: return "São Martinho (Países Baixos)"
+        case .MF: return "São Martinho (França)"
+        case .ST: return "São Tomé e Príncipe"
+        case .VC: return "São Vicente e Granadinas"
+        case .SN: return "Senegal"
+        case .SL: return "Serra Leoa"
+        case .RS: return "Sérvia"
+        case .SC: return "Seychelles"
+        case .SG: return "Singapura"
+        case .SY: return "Síria"
+        case .SO: return "Somália"
+        case .LK: return "Sri Lanka"
+        case .SD: return "Sudão"
+        case .SS: return "Sudão do Sul"
+        case .SE: return "Suécia"
+        case .CH: return "Suíça"
+        case .SR: return "Suriname"
+        case .SJ: return "Svalbard e Jan Mayen"
+        case .TH: return "Tailândia"
+        case .TW: return "Taiwan"
+        case .TJ: return "Tajiquistão"
+        case .TZ: return "Tanzânia"
+        case .TF: return "Terras Austrais e Antárticas Francesas (TAAF)"
+        case .IO: return "Território Britânico do Oceano Índico"
+        case .TL: return "Timor-Leste"
+        case .TG: return "Togo"
+        case .TK: return "Toquelau"
+        case .TO: return "Tonga"
+        case .TT: return "Trindade e Tobago"
+        case .TN: return "Tunísia"
+        case .TC: return "Turks e Caicos"
+        case .TM: return "Turquemenistão"
+        case .TR: return "Turquia"
+        case .TV: return "Tuvalu"
+        case .UA: return "Ucrânia"
+        case .UG: return "Uganda"
+        case .UY: return "Uruguai"
+        case .UZ: return "Usbequistão"
+        case .VU: return "Vanuatu"
+        case .VA: return "Vaticano"
+        case .VE: return "Venezuela"
+        case .VN: return "Vietnam"
+        case .VI: return "Virgens Americanas, Ilhas"
+        case .VG: return "Virgens Britânicas, Ilhas"
+        case .WF: return "Wallis e Futuna"
+        case .ZM: return "Zâmbia"
+        case .ZW: return "Zimbabwe"
+        }
+    }
+}
