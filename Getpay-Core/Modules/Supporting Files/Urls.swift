c773f416de7ae16a2f38326c56356e04c4b600e4
Leandro Lopes <leandrolopes.datum@getnet.com.br>
2021-04-03 23:52:18 -0300

    feat ( GPAY-5475 ): new target mock

--- Getpay-Core/Modules/Supporting Files/Urls.swift
+++ Getpay-Core/Modules/Supporting Files/Urls.swift
@@ -23,6 +23,7 @@ public enum Environment: String {
     
     case prod
     case hti
+    case mock
     
     var url: AppUrls {
         switch self {
@@ -33,6 +34,11 @@ public enum Environment: String {
                            portServicesURL: "https://servicosportais.getnet.com.br/")
         case .hti:
             return AppUrls(baseURL: "https://api-homologacao.getnet.com.br/getpay",
+                           issuer: "https://getsso-hom.getnet.com.br",
+                           transactionalURL: "",
+                           portServicesURL: "https://servicosportais-hti.getnet.com.br/")
+        case .mock:
+            return AppUrls(baseURL: "http://demo6462393.mockable.io",
                            issuer: "https://getsso-hom.getnet.com.br",
                            transactionalURL: "",
                            portServicesURL: "https://servicosportais-hti.getnet.com.br/")
